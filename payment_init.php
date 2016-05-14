bc8464b02091d262c68222c313f1db0be9b77f1f
thusitha madusanka <thusitha madusanka>
2016-05-14 13:48:05 +0530

    set file paths

--- payment_init.php
+++ payment_init.php
@@ -12,6 +12,33 @@
     <script src="js/bootswatch.js"></script>
 </head>
 <body>
+		<?php
+        error_reporting(E_ALL);
+        ini_set('display_errors', 0);
+        ?>
+
+        <?php include 'lib/au.com.gateway.client/GatewayClient.php'; ?>
+        <?php include 'lib/au.com.gateway.client.config/ClientConfig.php'; ?>
+        <?php include 'lib/au.com.gateway.client.component/RequestHeader.php'; ?>
+        <?php include 'lib/au.com.gateway.client.component/CreditCard.php'; ?>
+        <?php include 'lib/au.com.gateway.client.component/TransactionAmount.php'; ?>
+        <?php include 'lib/au.com.gateway.client.component/Redirect.php'; ?>
+        <?php include 'lib/au.com.gateway.client.facade/BaseFacade.php'; ?>
+        <?php include 'lib/au.com.gateway.client.facade/Payment.php'; ?>
+        <?php include 'lib/au.com.gateway.client.payment/PaymentInitRequest.php'; ?>
+        <?php include 'lib/au.com.gateway.client.payment/PaymentInitResponse.php'; ?>
+        <?php include 'lib/au.com.gateway.client.root/PaycorpRequest.php'; ?>
+        <?php include 'lib/au.com.gateway.client.utils/IJsonHelper.php'; ?>
+        <?php include 'lib/au.com.gateway.client.helpers/PaymentInitJsonHelper.php'; ?>
+        <?php include 'lib/au.com.gateway.client.utils/HmacUtils.php'; ?>
+        <?php include 'lib/au.com.gateway.client.utils/CommonUtils.php'; ?>
+        <?php include 'lib/au.com.gateway.client.utils/RestClient.php'; ?>
+        <?php include 'lib/au.com.gateway.client.enums/TransactionType.php'; ?>
+        <?php include 'lib/au.com.gateway.client.enums/Version.php'; ?>
+        <?php include 'lib/au.com.gateway.client.enums/Operation.php'; ?>
+        <?php include 'lib/au.com.gateway.client.facade/Vault.php'; ?>
+        <?php include 'lib/au.com.gateway.client.facade/Report.php'; ?>
+        <?php include 'lib/au.com.gateway.client.facade/AmexWallet.php'; ?>
 
 </body>
 </html>
