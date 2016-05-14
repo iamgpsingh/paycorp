63e865cfdfe2111f8aaf9093dabcbcf044f0149c
thusitha madusanka <thusitha madusanka>
2016-05-14 13:33:20 +0530

    add bootstrap Jquery

---
+++ payment_init.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Paycorp Init</title>
+    <link rel="stylesheet" href="css/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="css/bootswatch.min.css">
+    <link rel="stylesheet" href="css/jsonprettyprint.css">
+    <link rel="stylesheet" href="../pcw4/css/layout.css">
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootswatch.js"></script>
+</head>
+<body>
+
+</body>
+</html>
